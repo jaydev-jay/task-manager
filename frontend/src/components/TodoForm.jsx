5d966736664ae5b988bef0668825cdae69ac3494
jaydev-jay <jaydev6758@gmail.com>
2025-07-18 15:35:37 +0530

    Update TodoForm.jsx

--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = "https://task-manager-api-kohl-three.vercel.app";
 
 export default function TodoForm({ onTodoCreated }) {
   const [desc, setDesc] = useState("");
