2d97cf1c9c039dd8ce5bc2ba41aaa141994dd684
jaydev-jay <jaydev6758@gmail.com>
2025-07-18 16:29:50 +0530

    change

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL ="https://task-manager-api-kohl-three.vercel.app";
 
 export async function fetchWithAuth(path, method = 'GET', token, body = null) {
   const headers = {
